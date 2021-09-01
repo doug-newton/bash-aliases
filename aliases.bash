213e328c37252a351b7f16288fe0e59b63e9d9f5
Doug <27059467+doug-newton@users.noreply.github.com>
2021-09-01 17:57:04 +0200

    escape subcommand
    if you work overnight, you'll notice that the date command was only being run when the script was sourced
    
    you're welcome

--- aliases.bash
+++ aliases.bash
@@ -3,6 +3,6 @@
 alias log="git log --oneline"
 alias dog="git log --all --decorate --oneline --graph"
 alias dag="git log --all --decorate --graph"
-alias bog="git branch | grep $(date +%Y-%m-%d)"
+alias bog="git branch | grep \$(date +%Y-%m-%d)"
 alias snip-history="history | sed 's/^.......//g' | uniq | tail -n 20 > ~/source/repos/snippets/commands-\$(date +%Y%m%d-%H%M%S).md"
 alias view-snips="ls ~/source/repos/snippets | grep \"^commands\" | sed \"s/^/\~\/source\/repos\/snippets\//g\""
